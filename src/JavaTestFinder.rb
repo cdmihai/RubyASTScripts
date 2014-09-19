ec540b7494d23bb663c8dcd56e206010786c717b
Mihai Codoban <codobanmihai@gmail.com>
2014-09-18 17:23:29 -0700

    Remove hello world methods

--- src/JavaTestFinder.rb
+++ src/JavaTestFinder.rb
@@ -3,19 +3,7 @@ require "rubygems"
 require "json"
 
 class JavaTestFinder
-
-	def test_json()
-		string = '{"desc":{"someKey":"someValue","anotherKey":"value"},"main_item":{"stats":{"a":8,"b":12,"c":10}}}'
-		parsed = JSON.parse(string) # returns a hash
-
-		p parsed["desc"]["someKey"]
-		p parsed["main_item"]["stats"]["a"]
-	end
-
-	def add(a, b)
-		return a + b
-	end
-
+	
 	def find_tests(filePath)
 		json = get_json_from_file(filePath)
 
