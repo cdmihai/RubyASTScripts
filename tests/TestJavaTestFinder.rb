a28fbf1b2197a97ed369ab3efad40f31555b638b
Mihai Codoban <codobanmihai@gmail.com>
2014-09-18 17:59:22 -0700

    add setup method

--- tests/TestJavaTestFinder.rb
+++ tests/TestJavaTestFinder.rb
@@ -3,6 +3,10 @@ require "test/unit"
 
 class TestJavaTestFinder < Test::Unit::TestCase
 
+  def setup
+  	@testFinder = JavaTestFinder.new
+  end
+
   def test_no_test
   	assert_equal(0, @testFinder.find_tests("../testData/NoTest.java.json"))
   end
